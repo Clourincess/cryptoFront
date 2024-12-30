103d3287e0b9b4fae024360fc70420c98e7ab675
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-12-30 11:05:21 +0300

    fix fix

--- src/components/deposit_list.jsx
+++ src/components/deposit_list.jsx
@@ -4,30 +4,21 @@ import DepositListCard from "./deposit_list_card";
 const DepositList = () => {
   return (
     <VStack width="100%">
-      <HStack
-        alignSelf={"center"}
-        justify={"center"}
-        padding={"10px 20px"}
-        background={"#101010"}
-        borderRadius="28px"
-      >
-        <Text fontSize={"10px"} color={"white"}>
-          DEPOSIT LIST
+      <HStack alignSelf={"center"} justify={"center"}>
+        <Text fontSize={"9px"} color={"white"} marginTop={"10px"}>
+          DEPOSITS
         </Text>
-        <svg
-          width="12"
-          height="12"
-          viewBox="0 0 12 12"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="6" cy="6" r="6" fill="#2281A8" fill-opacity="0.5" />
-          <circle cx="6" cy="6" r="4" fill="#2281A8" />
-        </svg>
       </HStack>
-      <DepositListCard />
-      <DepositListCard />
-      <DepositListCard />
+      <VStack
+        align={"flex-start"}
+        width={"100%"}
+        marginTop={"50px"}
+        gap={"8px"}
+      >
+        <DepositListCard />
+        <DepositListCard />
+        <DepositListCard />
+      </VStack>
     </VStack>
   );
 };
