c00d07f25d226c71b9cb3f5c79ff6d071892b203
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-12-30 21:26:49 +0300

    fixed bugs

--- src/components/main_page/info_header.jsx
+++ src/components/main_page/info_header.jsx
@@ -6,7 +6,8 @@ import { useNavigate } from "react-router";
 import smile from "./../../assets/images/smile.JPG";
 import { useStores } from "../../store/store_context";
 import { observer } from "mobx-react-lite";
-import NoPhoto from "./../../assets/images/no_photo_user.png"
+import NoPhoto from "./../../assets/images/no_photo_user.png";
+import { useEffect, useState } from "react";
 const InfoHeaderMainPage = observer(() => {
   const navigate = useNavigate();
   const { GlobalVars } = useStores();
@@ -18,26 +19,77 @@ const InfoHeaderMainPage = observer(() => {
     first_name = tg.initDataUnsafe?.user?.first_name;
     last_name = tg.initDataUnsafe?.user?.last_name;
   }
+
+  let userId;
+
+  if (tg.initDataUnsafe.user != undefined) {
+    userId = GlobalVars.tg_id;
+  }
+  const [userInfo, setUserInfo] = useState([]);
+
+  useEffect(() => {
+    const getOneUser = async (tgId) => {
+      const response = await fetch(
+        `https://osiriscrypto.su:8008/getUserById?telegramm_id=${tgId}`,
+        {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+          },
+        }
+      );
+      const result = await response.json();
+      setUserInfo(result);
+    };
+    getOneUser(userId);
+  }, []);
+  let counter = 0;
+  let first_standart_deposit =
+    userInfo?.achievements?.first_standart_deposit == "Y" ? 20 : 0;
+  let first_master_deposit =
+    userInfo?.achievements?.first_master_deposit == "Y" ? 20 : 0;
+  let activate_someone_referal =
+    userInfo?.achievements?.activate_someone_referal == "Y" ? 20 : 0;
+  let activate_current_user_referal =
+    userInfo?.achievements?.activate_current_user_referal == "Y" ? 20 : 0;
+
+  counter =
+    first_standart_deposit +
+    first_master_deposit +
+    activate_someone_referal +
+    activate_current_user_referal;
+
+  counter == 20
+    ? GlobalVars.updateRank("IRON")
+    : counter == 40
+    ? GlobalVars.updateRank("SILVER")
+    : counter == 60
+    ? GlobalVars.updateRank("GOLD")
+    : counter == 80
+    ? GlobalVars.updateRank("DIAMOND")
+    : GlobalVars.updateRank("COAL");
+
   return (
     <HStack width={"100%"} justify={"space-between"}>
       <HStack align={"center"} onClick={() => navigate("/profile")}>
         <VStack justify={"center"}>
-          {tg.initDataUnsafe?.user?.photo_url !==""?
-          <Image
-            width={"33px"}
-            height={"33px"}
-            src={`${tg.initDataUnsafe?.user?.photo_url}`}
-            objectFit={"cover"}
-            borderRadius={"50%"}
-          />:
-          <Image
-            width={"33px"}
-            height={"33px"}
-            src={NoPhoto}
-            objectFit={"cover"}
-            borderRadius={"50%"}
-          />
-          }
+          {tg.initDataUnsafe?.user?.photo_url !== "" ? (
+            <Image
+              width={"33px"}
+              height={"33px"}
+              src={`${tg.initDataUnsafe?.user?.photo_url}`}
+              objectFit={"cover"}
+              borderRadius={"50%"}
+            />
+          ) : (
+            <Image
+              width={"33px"}
+              height={"33px"}
+              src={NoPhoto}
+              objectFit={"cover"}
+              borderRadius={"50%"}
+            />
+          )}
         </VStack>
 
         <VStack
