b0afae5a001bfc22732137a8af4ed4727f19ef6a
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-12-30 11:52:30 +0300

    calc income fixed

--- src/components/master_calc.jsx
+++ src/components/master_calc.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import { VStack, HStack, Text, Image, Input, Button } from "@chakra-ui/react";
 const arrow = (
   <svg
-    width="7"
+    width="6"
     height="8"
-    viewBox="0 0 7 8"
+    viewBox="0 0 6 8"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      d="M6.1167 4L0.0342223 7.4641L0.0342226 0.535898L6.1167 4Z"
-      fill="black"
+      d="M6 4.00064L1.85214e-07 7.46529L4.559e-07 0.535984L6 4.00064Z"
+      fill="white"
     />
   </svg>
 );
@@ -33,10 +33,10 @@ const MasterCalc = () => {
     <VStack
       borderRadius={"14px"}
       background={"rgba(20,20,20,0.6)"}
-      width={"170px"}
-      height={"356px"}
+      width={"166px"}
+      height={"422.5px"}
       padding={"10px"}
-      spacing={"10px"}
+      spacing={"0px"}
       justify={"space-between"}
       zIndex={10}
     >
@@ -47,22 +47,51 @@ const MasterCalc = () => {
         backgroundClip={"text"}
         fontSize={"10px"}
         textAlign={"center"}
+        fontWeight={"800"}
       >
         PREMIUN <br />
         ACCOUNT
       </Text>
-      <HStack width={"100%"} justify={"flex-start"}>
-        {arrow}
-        <Text fontSize={"10px"} color={"white"} alignSelf={"center"}>
-          ENTER DESIRED AMOUNT
-        </Text>
+      <HStack
+        width={"100%"}
+        justify={"space-between"}
+        alignItems={"center"}
+        marginTop={"34px"}
+        spacing={"0px"}
+      >
+        <HStack justify={"flex-start"}>
+          {arrow}
+          <Text fontSize={"10px"} color={"white"} alignSelf={"center"}>
+            ENTER DEPOSIT
+          </Text>
+        </HStack>
+
+        <svg
+          width="14"
+          height="14"
+          viewBox="0 0 14 14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M7 13.9688C10.866 13.9688 14 10.8417 14 6.98438C14 3.12701 10.866 0 7 0C3.13401 0 0 3.12701 0 6.98438C0 10.8417 3.13401 13.9688 7 13.9688Z"
+            fill="white"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M7.70963 7.6671C7.6615 7.67059 7.41344 7.68456 6.86 7.68456C6.41812 7.68456 6.10706 7.67146 5.99769 7.66622V7.66753C4.29669 7.59289 3.02706 7.29736 3.02706 6.94378C3.02706 6.59063 4.29669 6.2951 5.99769 6.21915V7.37332C6.10881 7.38117 6.42731 7.39995 6.86744 7.39995C7.3955 7.39995 7.66019 7.37812 7.70963 7.37375V6.22002C9.40713 6.29554 10.6737 6.59107 10.6737 6.94378C10.6737 7.29736 9.40713 7.59202 7.70963 7.6671ZM7.70963 6.09998V5.06716H10.0782V3.49219H3.62906V5.06716H5.99769V6.09954C4.07269 6.18772 2.625 6.56837 2.625 7.0241C2.625 7.47983 4.07269 7.86004 5.99769 7.94866V11.2584H7.70963V7.94778C9.63156 7.8596 11.0757 7.47939 11.0757 7.0241C11.0757 6.5688 9.63156 6.18859 7.70963 6.09998Z"
+            fill="#131313"
+          />
+        </svg>
       </HStack>
       <Input
-        placeholder="TYPE DEPOSIT AMOUNT"
+        placeholder="TYPE HERE"
         style={{
           backgroundColor: "black",
           fontSize: "10px",
-
+          marginTop: "10px",
+          textAlign: "left",
           borderRadius: "28px",
           width: "100%",
           padding: "5px 20px",
@@ -72,12 +101,13 @@ const MasterCalc = () => {
           setValues([Number(e.target.value), Number(values[1])]);
         }}
       />
-      <HStack>
-        {" "}
+      <HStack marginTop={"20px"}>
         <VStack
-          borderRadius={"14px"}
-          background={"#141414"}
-          padding={"2px"}
+          borderRadius={"10px"}
+          background={"black"}
+          padding={"6px"}
+          height={"62px"}
+          width={"38px"}
           onClick={() => {
             setHold([1.25, 0]);
           }}
@@ -87,14 +117,16 @@ const MasterCalc = () => {
           <Text fontSize={"9px"} color={"white"}>
             x1.25
           </Text>
-          <Text fontSize={"9px"} color={"white"} textAlign={"center"}>
-            1 MONTH HOLD
+          <Text fontSize={"7px"} color={"white"} textAlign={"center"}>
+            1 MONTH
           </Text>
         </VStack>
         <VStack
-          borderRadius={"14px"}
-          background={"#141414"}
-          padding={"2px"}
+          borderRadius={"10px"}
+          background={"black"}
+          padding={"6px"}
+          height={"62px"}
+          width={"38px"}
           onClick={() => {
             setHold([1.75, 1]);
           }}
@@ -104,14 +136,16 @@ const MasterCalc = () => {
           <Text fontSize={"9px"} color={"white"}>
             x1.75
           </Text>
-          <Text fontSize={"9px"} color={"white"} textAlign={"center"}>
-            1 MONTH HOLD
+          <Text fontSize={"7px"} color={"white"} textAlign={"center"}>
+            2 MONTH
           </Text>
         </VStack>
         <VStack
-          borderRadius={"14px"}
-          background={"#141414"}
-          padding={"2px"}
+          borderRadius={"10px"}
+          background={"black"}
+          padding={"6px"}
+          height={"62px"}
+          width={"38px"}
           onClick={() => {
             setHold([2.25, 2]);
           }}
@@ -121,15 +155,16 @@ const MasterCalc = () => {
           <Text fontSize={"9px"} color={"white"}>
             x2.25
           </Text>
-          <Text fontSize={"9px"} color={"white"} textAlign={"center"}>
-            1 MONTH HOLD
+          <Text fontSize={"7px"} color={"white"} textAlign={"center"}>
+            4 MONTH
           </Text>
         </VStack>
       </HStack>
       <Button
         borderRadius={"28px"}
         width={"86px"}
-        backgroundcolor={"white"}
+        background={"black"}
+        marginTop={"20px"}
         padding="10px"
         onClick={() => {
           setResult(values[0] * hold[0]);
@@ -145,13 +180,25 @@ const MasterCalc = () => {
           CALCULATE
         </Text>
       </Button>
-      <VStack width={"100%"} spacing={0}>
+      <VStack
+        width={"100%"}
+        spacing={0}
+        backgroundColor={"black"}
+        borderRadius={"10px"}
+        padding={"8px"}
+        marginTop={"30px"}
+      >
         <HStack width={"100%"} justify={"flex-start"}>
           <Text fontSize={"10px"} color={"white"} alignSelf={"center"}>
             ESTIMATED GENERATION:
           </Text>
         </HStack>
-        <Text fontSize={"30px"} color={"white"} alignSelf={"flex-start"}>
+        <Text
+          fontSize={"30px"}
+          color={"white"}
+          alignSelf={"flex-start"}
+          marginTop={"20px"}
+        >
           {Math.ceil(result, 2)}
         </Text>
         <Text fontSize={"9px"} color={"white"} alignSelf={"flex-start"}>
--- src/components/standart_calc.jsx
+++ src/components/standart_calc.jsx
@@ -32,53 +32,82 @@ const StandartCalc = () => {
   const [values, setValues] = useState([0, 0]);
   const [result, setResult] = useState(0);
   return (
-    <ColoredComponent background="radial-gradient(208.63% 103% at -30% 100%, #E39818 8%, #D05676 46.48%, #3A79F2 64.5%, #05B4FF 77.87%)">
-      <VStack width={"100%"} height={"100%"} spacing={"20px"}>
+    <ColoredComponent >
+      <VStack width={"100%"} height={"100%"} spacing={"0px"}>
         <Text
-          fontSize={"9px"}
+          fontSize={"10px"}
+          fontWeight={"800"}
           color={"black"}
           alignSelf={"center"}
           textAlign={"center"}
         >
           STANDARD <br /> ACCOUNT
         </Text>
-        <HStack width={"100%"} justify={"flex-start"}>
-          {arrow}
-          <Text fontSize={"10px"} color={"black"} alignSelf={"center"}>
-            ENTER DESIRED AMOUNT
-          </Text>
+        <HStack
+          width={"100%"}
+          justify={"space-between"}
+          alignItems={"center"}
+          marginTop={"34px"}
+        >
+          <HStack justify={"flex-start"}>
+            {arrow}
+            <Text fontSize={"9px"} color={"rgba(7,7,7,1)"} alignSelf={"center"}>
+              ENTER DEPOSIT
+            </Text>
+          </HStack>
+
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7 13.9688C10.866 13.9688 14 10.8417 14 6.98438C14 3.12701 10.866 0 7 0C3.13401 0 0 3.12701 0 6.98438C0 10.8417 3.13401 13.9688 7 13.9688Z"
+              fill="#070707"
+            />
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M7.70963 7.6671C7.6615 7.67059 7.41344 7.68456 6.86 7.68456C6.41812 7.68456 6.10706 7.67146 5.99769 7.66622V7.66753C4.29669 7.59289 3.02706 7.29736 3.02706 6.94378C3.02706 6.59063 4.29669 6.2951 5.99769 6.21915V7.37332C6.10881 7.38117 6.42731 7.39995 6.86744 7.39995C7.3955 7.39995 7.66019 7.37812 7.70963 7.37375V6.22002C9.40713 6.29554 10.6737 6.59107 10.6737 6.94378C10.6737 7.29736 9.40713 7.59202 7.70963 7.6671ZM7.70963 6.09998V5.06716H10.0782V3.49219H3.62906V5.06716H5.99769V6.09954C4.07269 6.18772 2.625 6.56837 2.625 7.0241C2.625 7.47983 4.07269 7.86004 5.99769 7.94866V11.2584H7.70963V7.94778C9.63156 7.8596 11.0757 7.47939 11.0757 7.0241C11.0757 6.5688 9.63156 6.18859 7.70963 6.09998Z"
+              fill="#05B4FF"
+            />
+          </svg>
         </HStack>
         <Input
-          placeholder="TYPE DEPOSIT AMOUNT"
+          placeholder="TYPE HERE"
           style={{
             backgroundColor: "black",
             fontSize: "10px",
-
+            textAlign: "left",
             borderRadius: "28px",
             width: "100%",
             padding: "5px 20px",
             color: "white",
+            marginTop: "10px",
           }}
           onChange={(e) => {
             setValues([Number(e.target.value), Number(values[1])]);
           }}
         />
-        <HStack width={"100%"} justify={"flex-start"}>
+        <HStack width={"100%"} justify={"flex-start"} marginTop={"34px"}>
           {arrow}
           <Text fontSize={"10px"} color={"black"} alignSelf={"center"}>
-            ENTER DESIRED TERM
+            ENTER DAYS NUMBER
           </Text>
         </HStack>
         <Input
-          placeholder="TYPE NUMBER OF DAYS"
+          placeholder="TYPE HERE"
           style={{
             backgroundColor: "black",
             fontSize: "10px",
-
+            marginTop: "10px",
             borderRadius: "28px",
             width: "100%",
             padding: "5px 20px",
             color: "white",
+            textAlign: "left",
           }}
           onChange={(e) => {
             setValues([Number(values[0]), Number(e.target.value)]);
@@ -88,6 +117,7 @@ const StandartCalc = () => {
           borderRadius={"28px"}
           width={"86px"}
           backgroundColor={"black"}
+          marginTop={"31px"}
           padding="10px"
           onClick={() => {
             setResult((values[0] + values[1]) * 1.25);
@@ -103,16 +133,23 @@ const StandartCalc = () => {
             CALCULATE
           </Text>
         </Button>
-        <VStack width={"100%"} spacing={0}>
+        <VStack
+          width={"100%"}
+          spacing={0}
+          backgroundColor={"black"}
+          padding={"8px"}
+          borderRadius={"10px"}
+          marginTop={"30px"}
+        >
           <HStack width={"100%"} justify={"flex-start"}>
-            <Text fontSize={"10px"} color={"black"} alignSelf={"center"}>
+            <Text fontSize={"10px"} color={"white"} alignSelf={"center"}>
               ESTIMATED GENERATION:
             </Text>
           </HStack>
-          <Text fontSize={"30px"} color={"black"} alignSelf={"flex-start"}>
+          <Text fontSize={"30px"} color={"white"} alignSelf={"flex-start"} marginTop={"20px"}>
             {Math.ceil(result, 2)}
           </Text>
-          <Text fontSize={"9px"} color={"black"} alignSelf={"flex-start"}>
+          <Text fontSize={"9px"} color={"white"} alignSelf={"flex-start"}>
             USDT TRC20
           </Text>
         </VStack>
