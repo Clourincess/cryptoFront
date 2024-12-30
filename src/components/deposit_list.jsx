30b0bc716a9a0ef0efec6eaa7b55b1edd1fa8a10
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-12-30 16:07:06 +0300

    added logic deposit list

--- src/components/deposit_list.jsx
+++ src/components/deposit_list.jsx
@@ -6,7 +6,7 @@ import { useStores } from "../store/store_context";
 const DepositList = () => {
   const { GlobalVars } = useStores();
   useEffect(() => {
-    GlobalVars.getAllDepositByUserName;
+    GlobalVars.getAllDepositByUserNameMaster();
   }, []);
   return (
     <VStack width="100%">
@@ -20,10 +20,44 @@ const DepositList = () => {
         width={"100%"}
         marginTop={"50px"}
         gap={"8px"}
+        alignItems={"center"}
       >
-        <DepositListCard />
-        <DepositListCard />
-        <DepositListCard />
+        {GlobalVars.master_deposits.length != 0 ? (
+          GlobalVars.master_deposits.map((item) => {
+            <DepositListCard />;
+          })
+        ) : (
+          <HStack
+            background="linear-gradient(69.76deg, #000000 46.83%, #370101 83.29%)"
+            height={"38px"}
+            width={"257px"}
+            justify={"center"}
+            align={"center"}
+            borderRadius={"14px"}
+          >
+            <Text color={"white"} fontSize={"9px"}>
+              YOU DON’T HAVE ANY ACTIVE DEPOSITS YET
+            </Text>
+            {
+              <svg
+                width="9"
+                height="10"
+                viewBox="0 0 9 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <ellipse
+                  cx="4.5"
+                  cy="4.54653"
+                  rx="4.5"
+                  ry="4.54653"
+                  fill="#D10000"
+                  fill-opacity="0.5"
+                />
+              </svg>
+            }
+          </HStack>
+        )}
       </VStack>
     </VStack>
   );
